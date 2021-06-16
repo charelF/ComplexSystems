20a853f0f3ebecc68eab0544801371e7939dc8a3
James Kingsbury <jameskingsbury919@gmail.com>
2021-06-16 17:59:18 +0200

    ca with plots

--- code/charel_ca_jk_alex.py
+++ code/charel_ca_jk_alex.py
@@ -59,12 +59,16 @@ def moving_average(x, w):
 
 #%%
 
-# pd = 0.25
-# pe = 0.02
-# ph = 0.18 # vary
-pd = 0.03
-pe = 0.01
-ph = 0.485 # vary
+# pd = 0.1
+# pe = 0.0001
+# ph = 0.1 # vary
+# pa = 1
+
+pd = 0.03 # active becomes inactive
+pe = 0.01 # probability of nontrader to enter the market
+ph = 0.0485  # probability that an active trader can turn one of his inactive neighbots into active
+pa = 0.5
+
 
 N0 = 1000
 N1 = 100
@@ -73,8 +77,8 @@ N1 = 100
 # a = 0.1
 # h = 0.1
 A = 1.8
-a = 0.1
-h = 0.1
+a = 1
+h = 1
 
 G = np.zeros(shape=(N0,N1))
 G[0] = np.random.choice(a=[-1,0,1], p=[pa/2, 1-pa, pa/2], size=N1, replace=True)
@@ -90,13 +94,19 @@ N[0] = B[0]  # noone has stock initially
 X = np.zeros(N0)
 S = np.zeros(N0)
 S[0] = initial_stock_price
-S_ma = np.zeros(N0)
-S_ma[0] = initial_stock_price
 # X[0] = 1
 
-DRIFT = 0.5
-MAXLOOKBACK = 3
-MA_T = 10
+S_ma10 = np.zeros(N0)
+S_ma10[0] = initial_stock_price
+S_ma20 = np.zeros(N0)
+S_ma20[0] = initial_stock_price
+S_ma30 = np.zeros(N0)
+S_ma30[0] = initial_stock_price
+
+MA_T_R= [2,4,10]
+
+DRIFT = 0.01
+MAXLOOKBACK = 4
 
 # each of the N1 agents has different treshold
 treshold = np.random.random(size=N1)*10
@@ -112,12 +122,18 @@ for t in range(N0-1):
         Xt += size * tmp
     X[t+1] = Xt/(10*N0)
     S[t+1] = S[t]*math.exp(X[t]) + DRIFT
-    S_ma[t+1] = np.mean(S[max(0, (t+1 - MA_T)):t+1])
+
+    for MA_T in MA_T_R:
+    
+        if MA_T == 2:
+            S_ma10[t+1] = np.mean(S[max(0, (t+1 - MA_T)):t+1])
+        elif MA_T == 4:
+            S_ma20[t+1] = np.mean(S[max(0, (t+1 - MA_T)):t+1])
+        else:
+            S_ma30[t+1] = np.mean(S[max(0, (t+1 - MA_T)):t+1])
 
 
     xi = np.random.uniform(-1, 1, size=Ncl)  # unique xi for each cluster k
-
-    res_grid = np.zeros((N1, N0-1))
 
     for i in range(N1):
         P[t+1,i] = P[t,i] + G[t,i]
@@ -131,46 +147,101 @@ for t in range(N0-1):
             k = coord2k[i]
             total = 0
             zeta = random.uniform(-1,1)  # sampled for each unique (k,i)
+
             change = (S[t] - initial_stock_price) / initial_stock_price
 
-            for j in k2coord[k]:  # for each coordinate in cluster k
-                eta = random.uniform(-1,1)  # different for each cell
-                sigma = G[t,j]
-                cluster_influence = A*xi[k]
-                member_influence = a*eta
-                total += ((cluster_influence + member_influence) * sigma)
 
-            choice = np.random.choice([0, 1, 2], p=[0, 0.5, 0.5]) 
-            if(choice==0):
-                # perf = percentage increase or decrease (pos or neg val)
-                performance = (N[t,i] - initial_account_balance) / initial_account_balance
-                # strat in [-1,1], high --> prefers buying, low --> prefers selling
-                lookback = min(t,MAXLOOKBACK)
-                strategy = np.mean(G[t-lookback:t+1,i])
-                # print(strategy)
-                # perf high --> continue with strat
-                # perf low --> change strat
-                # perf <0 --> switch sign of strat
-                bias = performance * strategy
-                trimmed_bias = max(-10, min(10, bias))
-                normalised_bias = 2 / (1 + math.exp(-2 * trimmed_bias)) - 1
-                self_influence = normalised_bias * h #* zeta
-                I = (1 / len(k2coord[k])) * total + self_influence 
-
-            elif(choice==1):
-                ma_diff = (S_ma[t+1] - S[t+1]) / (np.std(S[:t+1]))
-                ma_diff_norm = 2 / (1 + np.exp(-2 * ma_diff)) - 1
-                I = (1 / len(k2coord[k])) * total + self_influence + ma_diff_norm
-            else:
+            CLUSTER_DEPENDENCE_PROB = 0.5
+            if random.random() < CLUSTER_DEPENDENCE_PROB:
+                for j in k2coord[k]:  # for each coordinate in cluster k
+                    eta = random.uniform(-1,1)  # different for each cell
+                    sigma = G[t,j]
+                    cluster_influence = A*xi[k]
+                    member_influence = a*eta
+                    total += ((cluster_influence + member_influence) * sigma)
                 self_influence = h*zeta
-                I = (1 / len(k2coord[k])) * total + self_influence 
-            
-            p = 1 / (1 + math.exp(-2 * I))
 
-            if random.random() < p:
-                G[t+1,i] = 1
+                
+                I = (1 / len(k2coord[k])) * total + self_influence
+                p = 1 / (1 + math.exp(-2 * I))
+
+                if random.random() < p:
+                    G[t+1,i] = 1
+                else:
+                    G[t+1,i] = -1
+
             else:
-                G[t+1,i] = -1
+
+
+
+                 ## fair valuation 
+                ma_diff10 = (S_ma10[t+1] - S[t+1]) / np.std(S[:t+1]) # negative if stock>MA
+                ma_diff_norm10 = 2 / (1 + np.exp(-2 * ma_diff10)) - 1
+                ma_diff20 = (S_ma20[t+1] - S[t+1]) / np.std(S[:t+1]) # negative if stock>MA
+                ma_diff_norm20 = 2 / (1 + np.exp(-2 * ma_diff20)) - 1
+                ma_diff30 = (S_ma30[t+1] - S[t+1]) / np.std(S[:t+1]) # negative if stock>MA
+                ma_diff_norm30 = 2 / (1 + np.exp(-2 * ma_diff30)) - 1
+                
+                # RANDOM AGENT
+                AR_PROB = 0.5 # MA agent
+                AR_PERFORMANCE = 0.9 # portfolio agent
+    
+                RANDOM_AGENT = random.random()
+                
+                
+                if RANDOM_AGENT <= AR_PROB:
+                    VAL_RAND = np.random.normal(0,0.05)
+
+                    RATIONAL_RANDOM = random.random()
+                    
+                    p10 = 1 / (1 + math.exp(- ma_diff_norm10+VAL_RAND))
+                    p20 = 1 / (1 + math.exp(- ma_diff_norm20+VAL_RAND))
+                    p30 = 1 / (1 + math.exp(- ma_diff_norm30+VAL_RAND))
+                    
+                    if ((random.random() <= 0.8) and (G[t,i]==0)):
+                        G[t+1,i] = 0
+
+                    if RATIONAL_RANDOM <= 0.05:
+                        if random.random() <= p30:
+                            G[t+1,i] = +1
+                        else:
+                            G[t+1,i] = -1
+                    elif RATIONAL_RANDOM <= 0.3:
+                        if random.random() <= p20:
+                            G[t+1,i] = +1
+                        else:
+                            G[t+1,i] = -1
+                    elif RATIONAL_RANDOM <= 0.6:
+                        if random.random() <= p10:
+                            G[t+1,i] = +1
+                        else:
+                            G[t+1,i] = -1
+                    else:
+                        G[t+1,i] = np.random.choice([-1,1])
+                elif RANDOM_AGENT <= AR_PERFORMANCE:
+                    # perf = percentage increase or decrease (pos or neg val)
+                    performance = (N[t,i] - initial_account_balance) / initial_account_balance
+                    # strat in [-1,1], high --> prefers buying, low --> prefers selling
+                    lookback = min(t,MAXLOOKBACK)
+                    strategy = np.mean(G[t-lookback:t+1,i])
+                    bias = performance * strategy
+                    trimmed_bias = max(-10, min(10, bias))
+                    normalised_bias = 2 / (1 + math.exp(-2 * trimmed_bias)) - 1
+                    self_influence = normalised_bias * h #* zeta
+                    I = (1 / len(k2coord[k])) * total + self_influence 
+
+                    p = 1 / (1 + math.exp(-2 * I))
+                    if random.random() < p:
+                        G[t+1,i] = 1
+                    else:
+                        G[t+1,i] = -1
+
+                else:
+                    G[t+1,i] = np.random.choice([-1,0,1])
+
+
+
+
 
         # trader influences non-active neighbour to join
         if G[t,i] != 0:
@@ -239,14 +310,14 @@ cax3.get_yaxis().set_visible(False)
 
 
 ax2.plot(S, label="S")
-# Ws = [10,25,100]
-# for W in Ws:
-#     ax2.plot(np.arange(W-1, len(S)), moving_average(S, W), label=f"MA{W}")
+Ws = [10]
+for W in Ws:
+    ax2.plot(np.arange(W-1, len(S)), moving_average(S, W), label=f"MA{W}")
 ax2.grid(alpha=0.4)
+ax2.legend(ncol=len(Ws)+1)
 
 ax3.bar(np.arange(len(X)), X)
 ax3.grid(alpha=0.4)
-ax3.legend()
 
 ax4.set_xlabel("time")
 # ax2.set_ylabel("standardised log returns")
