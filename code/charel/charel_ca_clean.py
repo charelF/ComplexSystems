5ebe07f516ed2aab5dcc563117957dfbde8aff7e
Charel Felten <charel.felten@gmail.com>
2021-06-18 15:46:41 +0200

    charel clean potentially working

--- code/charel/charel_ca_clean.py
+++ code/charel/charel_ca_clean.py
@@ -57,16 +57,10 @@ def trunc(X, high, low):
 def moving_average(x, w):
     return np.convolve(x, np.ones(w), 'valid') / w
 
-def visualiseFAST(G, P, N, S, X, D):
-    fig, (ax1,ax2) = plt.subplots(ncols=1, nrows=2, figsize=(12,4))
-    ax1.imshow(G.T, cmap="bone", interpolation="None", aspect="auto")
-    ax2.semilogy(S)
-    plt.show()
-
-def visualiseNICE(G, P, N, S, X, D, T, U):
-    fig, (ax1,ax2,ax3,ax4,ax5,ax6,ax7) = plt.subplots(
-        ncols=1, nrows=7, figsize=(12,12), sharex=True, gridspec_kw = 
-        {'wspace':0, 'hspace':0.05, 'height_ratios':[1,2,1,1,1,1,1]}
+def visualiseNICE(G, P, N, S, X, D, T, U, C):
+    fig, (ax1,ax2,ax3,ax4,ax5,ax6,ax7,ax8) = plt.subplots(
+        ncols=1, nrows=8, figsize=(12,12), sharex=True, gridspec_kw = 
+        {'wspace':0, 'hspace':0.05, 'height_ratios':[1,2,1,1,1,1,1,1]}
     )
     im1 = ax1.imshow(G.T, cmap="bone", interpolation="None", aspect="auto")
     im4 = ax4.imshow(P.T, cmap="hot", interpolation="None", aspect="auto")
@@ -102,6 +96,9 @@ def visualiseNICE(G, P, N, S, X, D, T, U):
     cax7 = make_axes_locatable(ax7).append_axes('right', size=size, pad=0.05)
     cax7.get_xaxis().set_visible(False)
     cax7.get_yaxis().set_visible(False)
+    cax8 = make_axes_locatable(ax8).append_axes('right', size=size, pad=0.05)
+    cax8.get_xaxis().set_visible(False)
+    cax8.get_yaxis().set_visible(False)
 
     # for ax in (ax2,ax3):
     #     cax = make_axes_locatable(ax).append_axes('right', size=size, pad=0.05)
@@ -119,9 +116,20 @@ def visualiseNICE(G, P, N, S, X, D, T, U):
     ax3.grid(alpha=0.4)
 
     # if D.shape[1] < 25:
-    #     ax6.plot(D, color="black", alpha=0.3)
+    ax6.plot(np.mean(D[0],axis=1), color="C0", alpha=1, label="CA")
+    ax6.plot(np.mean(D[1],axis=1), color="C1", alpha=1, label="momentum")
+    ax6.plot(np.mean(D[2],axis=1), color="C2", alpha=1, label="invert")
+    ax6.plot(np.max(D[0],axis=1), ":", color="C0", alpha=1, label="CA")
+    ax6.plot(np.max(D[1],axis=1), ":", color="C1", alpha=1, label="momentum")
+    ax6.plot(np.max(D[2],axis=1), ":", color="C2", alpha=1, label="invert")
+    ax6.plot(np.min(D[0],axis=1), "--", color="C0", alpha=1, label="CA")
+    ax6.plot(np.min(D[1],axis=1), "--", color="C1", alpha=1, label="momentum")
+    ax6.plot(np.min(D[2],axis=1), "--", color="C2", alpha=1, label="invert")
     # ax6.plot(np.mean(D,axis=1), color="black", alpha=1)
-    # ax6.grid(alpha=0.4)
+    ax6.grid(alpha=0.4)
+    # ax6.legend()
+
+
     ax7.set_yscale("symlog")
     ax7.plot(T, label="stack")
     ax7.plot(U, label="called shares")
@@ -131,11 +139,10 @@ def visualiseNICE(G, P, N, S, X, D, T, U):
     # if D.shape[1] < 25:
     #     ax6.plot(D, color="black", alpha=0.3)
     # ax6.plot(np.mean(D,axis=1), color="black", alpha=1)
-    ax6.imshow(D.T, cmap="binary", interpolation="None", aspect="auto")
+    ax8.imshow(C.T, cmap="binary", interpolation="None", aspect="auto")
     # ax6.grid(alpha=0.4)
     
-
-    ax6.set_xlabel("time")
+    ax8.set_xlabel("time")
     # ax2.set_ylabel("standardised log returns")
     ax2.set_ylabel("close price")
     ax1.set_ylabel("agents")
@@ -143,13 +150,18 @@ def visualiseNICE(G, P, N, S, X, D, T, U):
     ax4.set_ylabel("portfolio")
     ax5.set_ylabel("net worth")
     ax6.set_ylabel("influence (I)")
+    ax7.set_ylabel("stack")
+    ax8.set_ylabel("margin calls")
 
     # fig.colorbar(im, cax=ax4)
 
     plt.tight_layout()
+    # plt.savefig("tmp.png", dpi=300)
     plt.show()
 
 #%%
+
+# =================== Parameters ================
 
 pd = 0.05
 pe = 0.01
@@ -157,16 +169,19 @@ ph = 0.05
 pa = 0.3
 
 N0 = 200
-N1 = 100
+N1 = 20
 
-A = 10
+A = 2
 a = 1
 h = 1
 
 # probabilities of investor types
-pi1 = 0.8  # original CA
-pi2 = 0.1  # momentum
-pi3 = 0.1  # invert / fundamental
+pi1 = 0.5  # original CA
+pi2 = 0.25  # momentum
+pi3 = 0.25  # invert / fundamental
+
+# max absolute influence
+max_I = 2
 
 initial_account_balance = 1000
 min_account_balance = 200
@@ -175,6 +190,8 @@ initial_stock_price = 100
 drift = 0  # not really working
 max_look_back = 10
 max_treshold = 1
+
+# ==================================
 
 G = np.zeros(shape=(N0,N1))
 G[0] = np.random.choice(a=[-1,0,1], p=[pa/2, 1-pa, pa/2], size=N1, replace=True)
@@ -188,7 +205,8 @@ B = np.zeros_like(G) # acc balance
 B[0] = initial_account_balance  # everyone start with 1000 money
 N[0] = B[0]  # noone has stock initially
 
-D = np.zeros_like(G)
+D = np.zeros((3, *G.shape))  # decision
+C = np.zeros_like(G)  # margin call
 
 X = np.zeros(N0)
 S = np.zeros(N0)
@@ -203,21 +221,8 @@ U = np.zeros(N0)
 stack = 0
 max_to_be_sold = N1
 
-
-investor_type = np.random.choice(a=[0,1,2], size=N1, replace=True,p = [pi1,pi2,pi3
-        1, 
-        .0, # momentum strategy
-        .0, # market inverter
-    ]
-)
-# investor_type = np.random.choice(
-#     a=[0,1,2], size=N1, replace=True,
-#     p = [
-#         .6, # original CA
-#         .3, # momentum strategy
-#         .1, # market inverter
-#     ]
-# )
+investor_type = np.random.choice(
+    a=[0,1,2], size=N1, replace=True, p = [pi1,pi2,pi3])
 
 for t in range(N0-1):
     Ncl, Nk, k2coord, coord2k = cluster_info(G[t])
@@ -237,11 +242,6 @@ for t in range(N0-1):
         to_be_sold = stack
         stack = 0
 
-    # print("-----------------")
-    # print("t         : ", t)
-    # print("Xt         : ", Xt)
-    # print("to_be_sold  : ", to_be_sold)
-
     Xt -= to_be_sold
     X[t+1] = Xt/(10*N0)
     S[t+1] = S[t]*math.exp(X[t]) + drift
@@ -256,78 +256,36 @@ for t in range(N0-1):
         N[t+1,i] = B[t,i] + (P[t,i]*S[t])
 
         if G[t,i] != 0:
-            # =================================================================
-
-            # original -------------------------------------------------------------------------------
-            # k = coord2k[i]
-            # total = 0
-            # zeta = random.uniform(-1,1)  # sampled for each unique (k,i)
-            # for j in k2coord[k]:  # for each coordinate in cluster k
-            #     eta = random.uniform(-1,1)  # different for each cell
-            #     sigma = G[t,j]
-            #     cluster_influence = A*xi[k]
-            #     member_influence = 0#a*eta
-            #     total += ((cluster_influence + member_influence) * sigma)
-            # self_influence = h*zeta
-            # I = (1 / len(k2coord[k])) * total + self_influence
-            # p = 1 / (1 + math.exp(-2 * I))
-
-            # same code but cleaner (only difference: no member influence) ----------------------------
-            # k = coord2k[i]
-            # zeta = random.uniform(-1,1)  # sampled for each unique (k,i)
-            # cluster_influence = A * xi[k] * np.mean(G[t,k2coord[k]])
-            # self_influence = h * zeta
-            # I = cluster_influence + self_influence
-            # p = 1 / (1 + math.exp(-2 * I))
-
-            # minimal version -------------------------------------------------------------------------
-            # k = coord2k[i]
-            # cluster_influence = A * trunc(np.mean(G[t,k2coord[k]]),1,-1)
-            # self_influence = h * trunc(G[t,i],1,-1)
-            # I = cluster_influence + self_influence
-            # p = 1 / (1 + math.exp(-2 * I))
-
-            # 3 agent model -------------------------------------------------------------------------
+            
             if investor_type[i] == 0 or investor_type[i]==2:
-                # agent # 1
                 k = coord2k[i]
-                zeta = random.uniform(-1,1)  # sampled for each unique (k,i)
-                cluster_influence = A * trunc(np.mean(G[t,k2coord[k]]),3,-3) * xi[k]
-                self_influence = h * trunc(G[t,i],3,-3) * zeta
-                I = cluster_influence + self_influence
-                p = 1 / (1 + math.exp(-2 * I))
+                zeta = random.uniform(-1,1)
+                cluster_influence = A * np.mean(G[t,k2coord[k]]) * xi[k]
+                self_influence = h * zeta
+                I = trunc(cluster_influence + self_influence, max_I, -max_I)
+
             if investor_type[i] == 1:
                 performance = (N[t,i] - initial_account_balance) / initial_account_balance
                 lookback = min(t,max_look_back)
                 strategy = np.mean(G[t-lookback:t+1,i])
-                bias = performance * strategy * 10
-                trimmed_bias = trunc(bias, 3, -3)
-                # trimmed_bias = max(-10, min(10, bias))
-                # normalised_bias = 2 / (1 + math.exp(-2 * trimmed_bias)) - 1
-                # self_influence = normalised_bias * h
-                self_influence = trimmed_bias * h
-                I = self_influence
-                p = 1 / (1 + math.exp(-2 * I))
+                bias = performance * strategy
+                self_influence = bias * h
+                I = trunc(self_influence * 10, max_I, -max_I)
+
             if investor_type[i] == 2:
                 change = (S[t] - initial_stock_price) / initial_stock_price
-                trigger = treshold[i] - abs(change)  # when they decide to inverse others
-                # stock goes up --> change = pos --> they inverse others --> their I = negative
-                if trigger < 0:
-                    p = 1-p
-                    # I = trunc(-change*5, 10, -10)
-                # p = 1 / (1 + math.exp(-2 * I))
+                trigger = treshold[i] - abs(change)
+                I = change * 100 * (1 if trigger > 0 else -1)
+                I = trunc(I, max_I, -max_I)
 
-
-            # =================================================================
-            # D[t,i] = I
+            p = 1 / (1 + math.exp(-2 * I))    
             if random.random() < p:
-                G[t+1,i] = trunc(round(I),2,1)
+                decision = trunc(round(I),max_I,1)
             else:
-                G[t+1,i] = trunc(-abs(round(I)),-1,-2)
-            # if random.random() < p:
-            #     G[t+1,i] = 1
-            # else:
-            #     G[t+1,i] = -1
+                decision = trunc(-abs(round(I)),-1,-max_I)
+
+            G[t+1,i] = decision
+            D[investor_type[i], t,i] = decision
 
         # trader influences non-active neighbour to join
         if G[t,i] != 0:
@@ -358,12 +316,8 @@ for t in range(N0-1):
                 G[t+1,i] = np.random.choice([-1,1])
 
     # margin call
-    # still_ok = N[t] > min_account_balance
-    # G[t+1] = G[t+1] * still_ok
-    # margin call
-    # still_ok = N[t] > min_account_balance
     margin_call = N[t] < min_account_balance
-    D[t+1] = margin_call
+    C[t+1] = margin_call
     # those that are margin called become inactive
     G[t+1] = G[t+1] * np.logical_not(margin_call) # those that are not remain
     P[t+1] = P[t+1] * np.logical_not(margin_call) # those that are not keep their portfolio
@@ -376,26 +330,15 @@ for t in range(N0-1):
     sum_margin_called = sum(margin_call)
     # these shares are sold at current price
     U[t+1] = sum_called_shares
-    # print(sum_called_shares)
     stack += sum_called_shares * sum_margin_called
-
-    # # margin call
-    # still_ok = N[t] > min_account_balance
-    # G[t+1] = G[t+1] * still_ok
-    # stack = 0
 
 
 final_trade = P[-1] * S[-1]
 B[-1] += final_trade
 N[-1] = B[-1]
 
-visualiseNICE(G,P,N,S,X,D,T,U)
-# visualiseFAST(G,P,N,S,X,D)
+visualiseNICE(G,P,N,S,X,D,T,U,C)
 
 # %%
 
 # %%
-
-# %%
-
-
