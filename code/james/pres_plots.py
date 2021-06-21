92d4ae5d13e9ca17f9910aee736fec7dfa1cce94
James Kingsbury <jameskingsbury919@gmail.com>
2021-06-21 16:22:30 +0200

    simulations

---
+++ code/james/pres_plots.py
@@ -0,0 +1,69 @@
+# %%
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import math
+import random
+from scipy import stats
+import statsmodels.api as sm
+
+from numba import jit
+
+import sys
+sys.path.append('../shared')
+from wednesdaySPEED import simulation
+
+# %%
+
+res = np.zeros((20, 30))
+for z in range(20):
+    G,P,N,S,X,D,T,U,C, initial_account_balance = simulation(trigger = False, bound = True, pd = 0.05, pe = 0.01,
+        ph = 0.0485, pa = 0.7, N0=1000, N1 = 100, A = 4, a=1, h=1, 
+        pi1 = 0.5, pi2 = 0.3, pi3 = 0.2)
+    h_res, q_vals = get_hurst_exponent(S)
+    res[z,:] = h_res*q_vals
+
+res_mean_ca = np.mean(res, axis=0)
+res_std_ca = np.std(res, axis=0)
+
+#%%
+
+df = pd.read_csv("../../data/all_world_indices_clean.csv")
+df_spx = df[["Date", "SPX Index"]]
+df_spx["Date"] = pd.to_datetime(df_spx["Date"], format='%d/%m/%Y')
+df_spx = df_spx.sort_values(by="Date")
+df_spx.reset_index(inplace=True)
+series_array = np.array(df_spx["SPX Index"])
+
+## identical to np.split but doesnt raise exception if arrays not equal length
+split = np.array_split(series_array, 6)
+res = np.zeros((6, 30))
+
+for i in range(len(split)):
+    h_res, q_vals = get_hurst_exponent(split[i])
+    res[i,:] = h_res*q_vals
+
+res_mean_sp = np.mean(res, axis=0)
+res_std_sp = np.std(res, axis=0)
+
+#%%
+
+fig, (ax1,ax2) = plt.subplots(
+    ncols=1, nrows=2, figsize=(12,8), sharex=True, gridspec_kw = {'wspace':0, 'hspace':0}
+)
+
+ax1.errorbar(q_vals, res_mean_ca, color="C4", yerr=res_std_ca, label='CA Gen')
+ax1.fill_between(q_vals, res_mean_ca + res_std_ca, res_mean_ca - res_std_ca)
+ax1.grid(alpha=0.2)
+ax1.set_ylabel(r"$q \cdot H(q)$")
+ax1.set_xlabel(r"$q$")
+ax1.legend()
+
+ax2.errorbar(q_vals, res_mean_sp, color="C6", yerr=res_std_sp, label='S&P500 Chunked')
+ax1.fill_between(q_vals, res_mean_sp + res_std_sp, res_mean_sp - res_std_sp)
+ax2.grid(alpha=0.2)
+ax2.set_ylabel(r"$q \cdot H(q)$")
+ax2.set_xlabel(r"$q$")
+plt.legend()
+plt.savefig("imgs/img_hurst_exp_double")
